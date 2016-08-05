853ae18e04280e0e2b54407f8aae3f08ed0de810
Roman Kulichkov <roman@kulichkov.pro>
2016-08-05 18:09:55 +0700

    Добавил главную страницу в git

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />
+        <meta name='yandex-verification' content='6829434758ff8743' />
+        <meta name="Keywords" content="планета агентство недвижимости, агентство недвижимости планета новосибирск, ан планета недвижимости, ан планета новосибирск, планета агентство недвижимости отзывы, ан планета"/> 
+        <?
+        //-->
+        // Для vk.com
+        ?>
+        <meta property="og:image" content="/img/logo.png" />
+        <?
+        //<--
+        ?>
+        <title>Корпорация АН «Планета» — все операции по недвижимости г. Новосибирск</title>
+        <!-- other style -->
+        <link href="/css/font-face.css" rel="stylesheet" type="text/css">
+        <link href="/css/template_styles.css" type="text/css" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Cuprum:400,400italic,700,700italic&amp;subset=cyrillic,latin-ext,latin" rel="stylesheet" type="text/css">
+        <link href="/css/colors.css" rel="stylesheet" type="text/css">
+        <link href="/css/developers.css" rel="stylesheet" type="text/css">
+        <!--[if lt IE 9]><script type="text/javascript" src="/js/html5shiv.js"></script><![endif]-->
+        <script type="text/javascript" src="/js/jquery1.8.2-min.js"></script>
+        <script type="text/javascript" src="/js/scripts.js"></script>
+        <script type="text/javascript" src="//yandex.st/share/share.js" charset="utf-8"></script>
+        <!--noindex-->
+            <?include_once("./inc/zopim.php");?>
+        <!--/noindex-->
+        <!-- OK Widget -->
+        <script>
+            !function (d, id, did, st) {
+                var js = d.createElement("script");
+                js.src = "//connect.ok.ru/connect.js";
+                js.onload = js.onreadystatechange = function () {
+                    if (!this.readyState || this.readyState == "loaded" || this.readyState == "complete") {
+                        if (!this.executed) {
+                            this.executed = true;
+                            setTimeout(function () {onOkConnectReady()}, 0);
+                        }
+                    }
+                }
+                d.documentElement.appendChild(js);
+            }(document);
+            
+            function onOkConnectReady() {
+                OK.CONNECT.insertGroupWidget("ok_group_widget","56895380652075","{width:250,height:230}");
+            }
+        </script>
+        <!-- /OK Widget -->
+        <!-- VK Widget -->
+        <script type="text/javascript" src="//vk.com/js/api/openapi.js?105"></script>
+        <script type="text/javascript">
+            VK.init({apiId: 3529769, onlyWidgets: true});
+            VK.Widgets.Group("vk_groups", {mode: 0, width: "250", height: "230", color1: 'FFFFFF', color2: '2B587A', color3: '5B7FA6'}, 65936243);
+            VK.Widgets.Like("vk_like", {type: "mini", pageTitle: "www.planetacorps.ru", height: 20, pageUrl: "http://www.planetacorps.ru" });
+        </script>
+        <!-- /VK Widget -->
+        <!-- FB Widget -->
+        <script>
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/ru_RU/all.js#xfbml=1&appId=574843562535620";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+        <!-- /FB Widget -->
+    </head>
+    <body class="theme-blue">
+        <div id="container" class="main">
+            <header>
+                <div id="header-in">
+                    <a id="logo" href="/">
+                        <?include_once("./inc/logo.php");?> 
+                    </a>
+                    <nav id="top-menu">
+                        <div style="clear:both">
+                            <ul>
+                                <li><a href="/construction/">Объекты</a></li>
+                                <li><a href="/partners/">Партнеры</a></li>
+                                <li><a href="/services/">Услуги</a></li>
+                                <li><a href="/job/">Вакансии</a></li>
+                                <li><a href="/contacts/">Контакты</a></li>
+                            </ul>
+                        </div>
+                        <div style="clear:both; padding-top:35px; padding-right:20px; padding-left:10px">
+                            <?include_once("./inc/like_top.php");?>
+                        </div>
+                    </nav>
+                    <?include_once("./inc/header-contacts.php");?>
+                </div>
+                <div class="wrap-c">
+                    <?include_once("./inc/header-image.php");?> 
+                </div>
+            </header>
+            <div id="container-in">
+                <section>
+                    <aside id="column-l">
+                        <?include_once("./inc/event.php");?>
+                        <div class="left-fixed">
+                            <div id="ok_group_widget"></div>
+                            <br />
+                            <div id="vk_groups"></div>
+                        </div>
+                    </aside>
+                    <div id="content">
+                        <h1>Добро пожаловать</h1>
+                        <p>Корпорация АН «Планета» – это группа операторов рынка недвижимости Новосибирска. Мы предоставляем информационные, организационные, юридические и рекламные услуги при купле-продаже и аренде недвижимости в Новосибирске. Каждый клиент - наша наивысшая ценность, он заслуживает индивидуальной работы, особого внимания, напряжения всех сил и мыслей, а также почтительного отношения. Для наших постоянных клиентов предусмотрена гибкая система скидок.</p>
+                        <!--noindex-->
+                            <?include_once("./inc/yandex-share.php");?>
+                        <!--/noindex-->
+                        <h2>Объекты от застройщиков</h2>
+                        <h3>Железнодорожный район</h3>
+                        <img src="/img/developers/1j.jpg"/>
+                        <p class="sub-table_link">
+                            <a href="/construction/">Посмотреть весь список объектов</a> <i>(обновление было: <?=date('d.m.Y')?>)</i>
+                        </p>
+                        <?/*?>
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td align="center"><a target="_blank" href="http://www.makehouse.pro" title="MakeHouse.pro — строительство домов в Новосибирске и Новосибирской области"><img src="/img/makehouse.jpg"></a></td>
+                                <td align="center"><a href="/construction/"><img src="/img/oasis_n.png"></a></td>
+                                <td align="center"><a href="/construction/"><img src="/img/kvartal_n.png"></a></td>
+                            </tr>
+                            <tr>
+                                <td align="center"><a href="/construction/"><img src="/img/logo-klenovia.jpg"></a></td>
+                                <td align="center"><a href="/construction/"><img src="/img/bavaria_n.png"></a></td>
+                                <td align="center"></td>
+                            </tr>
+                        </table>
+                        <?*/?>
+                        <p>Дополнительную информацию по приобритению жилья у застройщиков, можно получить по телефонам: т. 33-11-524, +7 (903) 931-15-24 Сидоркин Вячеслав.</p>
+                        <p>А также по адресу: ул. Крылова 36, офис 521б, вход со стороны ул. Семьи Шамшиных.</p>
+                        <h2>Банки - партнеры</h2>
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr valign="middle">
+                                <td align="center"><a href="/partners/"><img src="/img/top_head_top.png"></a></td>
+                                <td align="center"><a href="/partners/"><img src="/img/bank-gpb.gif"></a></td>
+                                <td align="center"><a href="/partners/"><img src="/img/logo-rb.gif"></a></td>
+                            </tr>
+                            <tr valign="middle">
+                                <td align="center"><a href="/partners/"><img src="/img/ab_logo.png"></a></td>
+                                <td align="center"><a href="/partners/"><img src="/img/deltacredit.png"></a></td>
+                                <td align="center"><a href="/partners/"><img src="/img/logo_vtb24.jpg"></a></td>
+                            </tr>
+                            <tr valign="middle">
+                                <td align="center"><a href="/partners/"><img src="/img/investtorgbank.png"></a></td>
+                                <td align="center"></td>
+                                <td align="center"></td>
+                            </tr>
+                        </table>
+                    </div>
+                </section>
+            </div>
+            <footer>
+                <div>
+                    <nav id="footer-menu">
+                        <?include_once("./inc/footer-menu.php");?>
+                    </nav>
+                    <?include_once("./inc/copyright_footer.php");?>
+                </div>
+            </footer>
+        </div>
+        <!--noindex-->
+            <?include_once("./inc/yandex-metrika.php");?>
+        <!--noindex-->
+    </body>
+</html>
