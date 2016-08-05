4e3e3e9fb466a8998d2f230a07e1d898c24aadef
Roman Kulichkov <roman@kulichkov.pro>
2016-08-05 19:18:47 +0700

    Правка стилей и событий.

---
+++ inc/event.php
@@ -0,0 +1,59 @@
+<h3>События компании</h3>
+<ul class="left-news">
+    <li>
+      <div class="news-item-r">
+        <div>
+          <div style="color:#ccc;">02 июнь 2016</div>
+          <div>У компании «Планета» появился новый партнер ОАО АКБ «Инвестторгбанк».</div>
+          <div style="clear:both"></div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="news-item-r">
+        <div>
+          <div style="color:#ccc;">15 мая 2015</div>
+          <div>Подписан договор на продажу квартир в жилом комплексе «Кленовая аллея».</div>
+          <div style="clear:both"></div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="news-item-r">
+        <div>
+          <div style="color:#ccc;">28 февраля 2015</div>
+          <div>Подписан договор на продажу квартир в жилом комплексе «Оазис».</div>
+          <div style="clear:both"></div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="news-item-r">
+        <div>
+          <div style="color:#ccc;">27 февраля 2015</div>
+          <div>Подписан договор на продажу квартир в жилом комплексе «Бавария».</div>
+          <div style="clear:both"></div>
+        </div>
+      </div>
+    </li>
+    <?/*?>
+    <li>
+      <div class="news-item-r">
+        <div>
+          <div style="color:#ccc;">24 февраля 2013</div>
+          <div>Подписан договор на продажу квартир от СК «Приборкомплекс».</div>
+          <div style="clear:both"></div>
+        </div>
+      </div>
+    </li>
+    <li>
+      <div class="news-item-r">
+        <div>
+          <div style="color:#ccc;">24 февраля 2013</div>
+          <div>Подписан договор на продажу квартир в жилом комплексе «Светлая роща».</div>
+          <div style="clear:both"></div>
+        </div>
+      </div>
+    </li>
+    <?*/?>
+</ul>
